be09fb1eb14c83bd0deb2ede79f4ae10b34c488c
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2021-12-19 12:09:52 +0600

    colorMode added header v1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,45 @@ import React from "react";
 import styles from "@styles/Header.module.scss";
 
 function Header() {
+  function changeColorMode(mode: "bw" | "color") {
+    if (process.browser) {
+      const docElement = document.documentElement;
+      if (mode == "bw") {
+        docElement.classList.add("dark");
+      } else docElement.classList.remove("dark");
+    }
+  }
+  ///handleColorModeShift("bw");
   return (
-    <header className={`${styles.header} px-64`}>
-      <div className={styles.container}>
-        <div className={styles.nav}>
-          <div className={styles.logoWrapper}>
+    <header className={`${styles.header} px-4 3xl:px-64 `}>
+      <div className={`${styles.container}`}>
+        <div className={`${styles.nav} h-[4.2rem] lg:h-32`}>
+          <div className={`${styles.logoWrapper} lg:text-[25px] `}>
             <a href="/">
-              <span>REALLY</span>
-              <span>BEYOND</span>
+              <span className="text-green dark:text-black">REALLY</span>
+              <span className="text-lightGreen dark:text-gray-400">BEYOND</span>
             </a>
           </div>
           <nav>
-            <ul>
-              <li className={styles.color}>COLOR</li>
-              <li className={styles.bw}>B&W</li>
+            <ul className="hidden lg:flex">
+              <li
+                className={`text-orange border-b border-orange dark:text-gray-400 dark:border-none`}
+                onClick={() => changeColorMode("color")}
+              >
+                COLOR
+              </li>
+              <li
+                className="text-lightBlack hover:border-b dark:border-b border-lightBlack"
+                onClick={() => changeColorMode("bw")}
+              >
+                B&W
+              </li>
             </ul>
           </nav>
-          <a href="#footer" className={styles.contactUs}>
+          <a
+            href="#footer"
+            className="btn-primary text-[14px] lg:text-[16px]  px-[1.75rem] py-[.68rem] lg:px-10 lg:py-4 dark:bg-black"
+          >
             {" "}
             Contact us{" "}
           </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,46 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#094367",
+        lightBlack: "#212121",
+        lightOrange: "#F0E3D8",
+        orange: "#ff6d42",
+        green: "#348c41",
+        lightGreen: "#a3d8b0",
+      },
+      screens: {
+        "3xl": "1600px",
+      },
+    },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      textColor: ["hover"],
+      opacity: ["after"],
+      margin: ["last"],
+    },
+  },
+  plugins: [
+    plugin(function ({ addComponents, addVariant }) {
+      addComponents({
+        ".btn-primary": {
+          //fontSize: "16px",
+          lineHeight: "21px",
+          fontFamily: "Poppins Semibold",
+          padding: "18px 42px",
+          backgroundColor: "#094367",
+          color: "#fff",
+          display: "inline-block",
+          "&:hover": {
+            backgroundColor: " #ff6d42",
+          },
+        },
+      });
+    }),
+  ],
 };
