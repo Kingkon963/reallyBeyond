fc7d67ea2a4d6f04d66aea6fadbf5c5f0d59a908
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2021-12-28 15:55:46 +0600

    Menu 1440

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,10 +7,12 @@ import keyGen from "src/utils/genKey";
 function Info({ title, value }: { title: String; value: String }) {
   return (
     <div className="flex flex-col gap-1 justify-start w-full">
-      <span className="text-sm text-[#878787] font-poppinsSemibold uppercase">
+      <span className="text-sm lg:text-[12px] text-[#878787] font-poppinsSemibold uppercase">
         {title}
       </span>
-      <span className="font-poppinsRegular text-2xl">{value}</span>
+      <span className="font-poppinsRegular text-2xl lg:text-[20px]">
+        {value}
+      </span>
     </div>
   );
 }
@@ -57,12 +59,12 @@ function Menu() {
   return (
     <div className="bg-blue dark:bg-black text-white h-full  flex justify-center pt-10 lg:pt-32">
       <div className="container">
-        <div className="grid lg:grid-cols-2 pb-10 lg:pb-0">
-          <div className="flex flex-col gap-10 lg:border-r">
+        <div className="grid lg:grid-cols-12  pb-10 lg:pb-0">
+          <div className="lg:col-span-6 lg:mr-10 flex flex-col gap-6 lg:gap-12 lg:border-r">
             {menuItems.map((item) => {
               return (
                 <span
-                  className={`font-poppinsRegular text-2xl lg:text-4xl dark:text-[#878787] dark:hover:text-white hover:text-orange transition-none ${
+                  className={`font-poppinsRegular text-2xl lg:text-[30px] dark:text-[#878787] dark:hover:text-white hover:text-orange whitespace-nowrap transition-none ${
                     active === item.title ? "text-orange dark:text-white" : ""
                   }`}
                   onClick={() => setActiveLink()}
@@ -73,11 +75,11 @@ function Menu() {
               );
             })}
           </div>
-          <div className="flex flex-col gap-10 justify-center items-center pt-10 lg:pt-0 pb-10 lg:pb-0 border-t mt-10 lg:mt-0 lg:border-t-0 lg:px-24">
-            <h1 className="font-poppinsLight text-orange dark:text-white text-5xl lg:text-7xl w-full">
+          <div className="lg:col-span-6 lg:pl-40 flex flex-col gap-10 justify-center items-end pt-10 lg:pt-0 pb-10 lg:pb-0 border-t mt-10 lg:mt-0 lg:border-t-0">
+            <h1 className="font-poppinsLight text-orange dark:text-white text-5xl lg:text-[60px] w-full whitespace-nowrap">
               Get in Touch
             </h1>
-            <div className="flex flex-col gap-10 w-full">
+            <div className="flex flex-col gap-10 w-full lg:mb-6">
               <Info title="email" value="info@reallybeyond.com" />
               <Info title="Website" value="www.reallybeyond.com" />
               <Info title="WhatsApp/Telegram" value="+923484660336" />
