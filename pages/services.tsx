97085f78acc081c8ed857177f253a6d9117f756b
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2022-01-19 20:56:38 +0600

    ServiceCard: list foxed

--- pages/services.tsx
+++ pages/services.tsx
@@ -19,8 +19,33 @@ const ServicesPage: NextPage = () => {
         <div className="container pt-[67px] mb-[200px]">
           <InfinityHeader gap={35}>Services</InfinityHeader>
 
-          <div className="mt-[43px]">
+          <div className="mt-[43px] flex flex-col gap-[40px] lg:gap-[60px]">
             <ServiceCard title="Brand Identity" number={1}>
+              <div className="w-[298px] xsp:w-[358px] lg:w-auto mx-[12px] mt-[22px]">
+                <span className="text-[18px] text-green dark:text-black">
+                  Realization of your ideas and birth of your brand.
+                </span>
+                <p className="mt-0 text-[14px] lg:w-[498px] 3xl:w-[631px] leading-snug">
+                  Colors, Marks, Fonts, Shapes - All is created during the brand
+                  identity development and a personality of your brand comes to
+                  life -It is a true sense of accomplishment to see how it looks
+                  what you have been imagining from days or months. We make sure
+                  to create the assets that you love to see.
+                </p>
+                <h2 className="text-[18px] text-green dark:text-black mt-5">
+                  Process:
+                </h2>
+                <ul className="text-[14px] lg:ml-[17px] 3xl:ml-[19px] relative pt-1 list-outside list-[square]">
+                  <ListItem>Color Palette Selection</ListItem>
+                  <ListItem>Fonts Selection</ListItem>
+                  <ListItem>Sketches</ListItem>
+                  <ListItem>Logo Draft</ListItem>
+                  <ListItem>Revisions</ListItem>
+                  <ListItem>Polishing and Final Touches</ListItem>
+                </ul>
+              </div>
+            </ServiceCard>
+            <ServiceCard title="UX/UI & Graphic Design" number={2}>
               <div className="w-[298px] xsp:w-[358px] lg:w-auto mx-[12px] mt-[22px]">
                 <span className="text-[18px] text-green dark:text-black">
                   Realization of your ideas and birth of your brand.
