a3145ca80bd2ad67437f79469d28f857e392a56e
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2022-01-20 10:48:26 +0600

    blog page header fixed

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -14,11 +14,14 @@ const BlogPage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div className="container relative pt-24 pb-12 lg:py-24 lg:flex lg:flex-col lg:items-center ">
+        <div
+          className="container relative pt-24 pb-12 lg:py-[75px]
+         lg:flex lg:flex-col lg:items-center"
+        >
           <div className="relative mb-[62px] lg:mb-24 lg:w-[904px]">
             <span
               className="uppercase font-openSansBold text-[#7D7D7D] 
-            text-[12px] absolute left-0 -top-10"
+            text-[12px] absolute left-0 -top-10 lg:top-0"
             >
               Learning Constantly. Exploring Opportunities.
               <br className="sm:hidden" /> Always Improving
@@ -26,7 +29,7 @@ const BlogPage: NextPage = () => {
             <InfinityHeader gap={11}>Our Thoughts</InfinityHeader>
             <span
               className="uppercase font-openSansBold text-[#7D7D7D] 
-              text-[12px] absolute mt-[10px] lg:mt-0 lg:top-[86px] lg:left-[259px]"
+              text-[12px] absolute mt-[10px] lg:mt-0 lg:-bottom-[20px] lg:left-[259px]"
             >
               Here is all that we think and can't resist
               <br className="sm:hidden" /> sharing it with you
